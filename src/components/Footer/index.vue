1d4631512278b3dc3b3f26d9bc9c26b6cbae551c
liguangda1996 <1329095679@qq.com>
2020-11-28 08:27:22 +0800

    feat:footer-bottom

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,6 +1,23 @@
 <template>
 <div>
-    <p>Footer</p>
+    <div id="footer-bottom">
+        <div>
+            <a href="#">联系我们</a> |
+            <a href="#">关于我们</a> |
+            <a href="#">商家入驻</a> |
+            <a href="#">营销中心</a> |
+            <a href="#">友情链接</a> |
+            <a href="#">关于我们</a> |
+            <a href="#">友情链接</a> |
+            <a href="#">营销中心</a> |
+        </div>
+        <div>
+            地址：北京市昌平区宏福科技园综合楼6楼
+        </div>
+        <div>
+            京ICP备19006430号
+        </div>
+    </div>
 </div>
 </template>
 
@@ -11,5 +28,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+#footer-bottom{
+    height: 80px;
+    background-color:rgb(234,234,234);
+    padding: 20px;
+}
+#footer-bottom div{
+    height: 26px;
+    font-size: 12px;
+    text-align: center;
+}
+#footer-bottom div a{
+    text-decoration: none;
+    color: #333;
+}
 </style>
